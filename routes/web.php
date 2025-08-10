a2b22c829b1b3eb45c07fffb5d4e98a8f5c3997c
rotullima <kusnias743@gmail.com>
2025-08-11 01:18:45 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\HomeController;
 
-// Home (root) -> our modular Home page
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Dashboard tetap ada, tapi pindah ke /dashboard (sama props seperti sebelumya)
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard', [
         'name' => 'lang',
